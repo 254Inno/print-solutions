e4a2aaa0f4a67ac47ca0e02e60a65ead5cab437a
Innocent nyakangi <innocentnyakangi@gmail.com>
2023-07-23 23:35:17 +0300

    made the website responsive

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,59 @@
 ///-------------------DOM selectors
+const body = document.querySelector('body');
+
+function quoteMechanism(){
+
+   const quoteWindow = `<div class="quote-gen hide-checkbox">
+   <div class="quote-gen__container">
+   <div class="quote-gen__close">
+       <svg class="quote-gen__close--icon">
+           <use xlink:href="sprite.svg#circle-x"></use>
+       </svg>
+   </div>
+   <header class="quote-gen__head">
+       Quote generator
+
+       <span></span>
+   </header>
+
+   <div class="quote-gen__input--dropdown">
+       <select class="select" name="material" id="material">
+           <option value="Banner" selected>Banner</option>
+           <option value="Sticker">Sticker</option>
+           <option value="WindowGraphics" >Window graphics</option>
+           <option value="ReflectiveSticker">Reflective sticker</option>
+           <option value="ReflectiveBanner" >Reflective banner</option>
+           <option value="Satin" >Satin</option>
+           <option value="No cut" disabled >No cut</option>
+           <option value="clearSticker" disabled >Clear Sticker</option>
+       </select>
+   </div>
+   <div class="quote-gen__input--measurement">
+       <input type="number" placeholder="Width in meters" class="quote-gen__input--width" required>
+       <input type="number" placeholder="Hight in meters" class="quote-gen__input--height" required>
+   </div>
+   <div class="quote-gen__input-checkbox">
+       <label class="quote-gen__input--checkbox"><p>Tick if not designed</p>
+           <input type="checkbox" class="checkbox">
+           <span class="checkmark"></span>
+       </label>
+       </div>
+   <div class="quote-gen__input--btn quote-btn">
+       <input type="button" class="quote-gen__input-btn quote-btn btn" value="Generate">
+   </div>
+
+   <div class="quote-gen__total">
+      <p>Total</p> <span class="total-value">Ksh. 0.00</span>
+   </div>
+</div>
+
+</div>`
+
+   body.insertAdjacentHTML('beforeend', quoteWindow)
+   
+}
+
+quoteMechanism();
 
 const quotation = document.querySelector('.quote-gen');
 const getQuotation = document.querySelector('.nav__top--quote');
@@ -9,6 +64,13 @@ const materialWidth = document.querySelector('.quote-gen__input--width')
 const materialHeight = document.querySelector('.quote-gen__input--height')
 const checkBox = document.querySelector('.checkbox')
 const totalCoast = document.querySelector('.total-value')
+const mobileLinks = document.querySelector('.nav__mobile--links')
+const mobileBars = document.querySelector('.mobileBars')
+
+
+//----------Quote gen generator
+
+
 
 //-------Calculating Quotation
 
@@ -48,12 +110,25 @@ totalCoast.textContent = `Ksh. ${((perMeterSqrCoast * calcWidth * calcHeight) + 
 
 ///------------Event listeners
 
+
+// quote opener
 getQuotation.addEventListener('click', function(){
    quotation.classList.toggle('hide-checkbox')
 })
+
+// quotation closing
 closeQuotation.addEventListener('click', function(){
    quotation.classList.toggle('hide-checkbox')
 })
 
-///
+/// quotation calc
 quoteBtn.addEventListener('click', quotationCalc)
+
+/// mobile opening and closing
+
+mobileBars.addEventListener('click', () => {
+   mobileLinks.classList.toggle('hide')
+})
+mobileLinks.addEventListener('click', () => {
+   mobileLinks.classList.toggle('hide')
+})
